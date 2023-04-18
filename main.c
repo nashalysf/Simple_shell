491b875f66c6395cbf5570599eaae723d47cca78
nashalysf <nashalysf@gmail.com>
2023-04-18 16:28:44 +0000

    setting header file with initial prototypes and libraries and started main to init and display shell

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "simple_shell.h"
+/**
+ * main - gets user input and calls
+ * function(s) to preform command
+ * @ac: argument count
+ * @argv: argument vector (array)
+ * Return: 0. for success
+ */
+int main(int ac, char **argv)
+{
+/*
+ * lineptr- stores address of buffered input
+ * n- stores allocated input size in bytes
+ * stdin - std input used as stream of data source
+ * free()- must be freed by dev
+ */
+	char *lineptr;
+	size_t n = 0;
+
+/* prompt */
+	char *prompt = "Nadrian~$ ";
+
+	printf("%s", prompt);
+
+	(void)ac;
+	(void)argv;
+
+/* copies, displays, and realeases input */
+	getline(&lineptr, &n, stdin);
+	printf("%s\n", lineptr);
+	free(lineptr);
+	return (0);
+}
--- simple_shell.h
+++ simple_shell.h
@@ -8,9 +8,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdarg.h>
+#include <sys/types.h>
 
 /* STRUCTURES AND TYPEDEF */
-
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)));
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 /* PROTOTYPES */
 
 #endif
