89685334f99c5f94443b3a0f98ffd80fd645f17a
nashalysf <nashalysf@gmail.com>
2023-04-18 14:56:59 +0000

    adding header file base

---
+++ simple_shell.h
@@ -0,0 +1,16 @@
+#ifndef _SHELL_
+#define _SHELL_
+
+/* LIBRARIES */
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+/* STRUCTURES AND TYPEDEF */
+
+/* PROTOTYPES */
+
+#endif
